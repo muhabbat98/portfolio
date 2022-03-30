9ac37329adf0e069fca5ff03c98b8365a70b0723
muhabbat98 <55285047+muhabbat98@users.noreply.github.com>
2022-03-30 18:05:19 +0500

    carusel

--- server.js
+++ server.js
@@ -1,37 +1,59 @@
 const http = require( 'http' );
-
+const fs = require( 'fs' );
+var path = require('path');
 const PORT = process.env.PORT || 3000;
-const accountSid = 'AC4652f65526467651e4e7a514d995a97b';
-const authToken = '952869454b310db5c0e6696596549d85';
 
-const fs = require('fs');
-const index = fs.readFileSync( 'index.html' );
-const static = require('node-static');
+const accountSid = 'AC4652f65526467651e4e7a514d995a97b';
+const authToken = '6ba81e331f6a9e6e7461e788a1f08e6c';
+
 const client = require( 'twilio' )( accountSid, authToken );
 
-const fileServer = new static.Server( './public' );
 
 const server = http.createServer( ( req, res ) =>
 {
-    // fileServer.serve( req, res )
-    // fileServer.serve(req, res)
     
-    req.on( 'data', (e) =>
+    if(req.url === "/"){
+     req.on( 'data', (e) =>
     {
-        let bodyData = e.toString( 'utf-8' )
+        let bodyData = e.toString( 'utf-8' ).slice(e.toString( 'utf-8' ).indexOf('=')+1).replace('+',' ')
+   
+         if ( bodyData.trim().length )
+         {
+             client.messages.create( {
+                body: bodyData,
+                from: '+13252405955',
+                to: '+998909903391'  
+                
+             } ).then( ( m ) => console.log( 'id', m.sid ) )
+        }
+     } );
+        fs.readFile( "index.html", "utf-8", function ( err, html )
+        {
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(html);
+        });
+    }else if(req.url?.match("\.css$")){
+        var cssPath = path.join( __dirname, req.url );
+        var fileStream = fs.createReadStream(cssPath, "utf-8");
+        res.writeHead(200, {"Content-Type": "text/css"});
+        fileStream.pipe(res);
 
-        client.messages.create( {
-            body: bodyData.slice(bodyData.indexOf('=')+1).replace('+',' '),
-            from: '+13252405955',
-            to: '+998909903391'  
-            
-        }).then((m)=>console.log(m.sid))
-    } )
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end( index );
-     req.addListener('end', function () {
-        fileServer.serve(req, res);
-    }).resume();
+    }else if(req.url?.match("\.png$")){
+        var imagePath = path.join(__dirname,  req.url);
+        var fileStream = fs.createReadStream(imagePath);
+        res.writeHead(200, {"Content-Type": "image/png"});
+        fileStream.pipe(res);
+    }
+    else if(req.url?.match("\.js$")){
+        var jsPath = path.join(__dirname,  req.url);
+        var fileStream = fs.createReadStream(jsPath);
+        res.writeHead(200, {"Content-Type": "text/javascript"});
+        fileStream.pipe(res);
+    }else{
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end("No Page Found");
+    }
+   
 });
 
 
